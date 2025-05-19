49c6b069235bb8c4349892a7a475016b732879d1
ratbekd <96930059+ratbekd@users.noreply.github.com>
2025-05-19 14:13:11 +1000

    Add files via upload

--- siv_regression.R
+++ siv_regression.R
@@ -4,14 +4,47 @@
 #' @param Y Name of the dependent variable.
 #' @param X Name of the endogenous variable.
 #' @param H Vector of exogenous variable names.
-#' @param reps Number of bootsrap loops.
-#' @return A list containing OLS and IV regression results.
+#' @param reps Number of bootstrap loops.
+#' @return A list containing SIV regression results.IV1- a simple SIV, IV2- a parametric SIV, IV3- a non-parametric SIV, citable- a table of confidence intervals.
 #' @export
+#' @importFrom stats  predict
+#' @importFrom stats  complete.cases
+#' @importFrom stats  fitted
+#' @importFrom stats pf
+#' @importFrom stats pchisq
+#' @importFrom stats sum
+#' @importFrom stats sumsq
+#' @importFrom stats var
+#' @importFrom stats as.formula
+#' @importFrom stats lm
+#' @importFrom stats cor
+#' @importFrom stats cov
+#' @importFrom stats residuals
+#' @importFrom stats df.residual
+#' @importFrom stats qt
+#' @importFrom stats sign
+#' @importFrom stats rnrom
+#' @importFrom stats sd
+#' @importFrom stats mean
+#' @importFrom stats abs#'
 #' @examples
 #' df <- wooldridge::mroz  # Use sample data set
 #' data <- df[complete.cases(df), ]  # Remove missing values
 #' attach(data)
-#' result <- siv_regression(data, "hours", "lwage", c("educ", "age", "kidslt6", "kidsge6", "nwifeinc"), reps=5)
+#' H0 <- data.frame(hours, lwage, educ, age, kidslt6, kidsge6, nwifeinc)
+# Vector of variables used in the regression. Firts var is outcome, the second var is endogenous regressor
+#' Y <- as.character(colnames(H0))[1] ###OUTCOME variable
+#' X <-as.character(colnames(H0))[2] ###ENDOEGENOUS variable
+#' H<- as.character(colnames(H0))[-(1:2)] #
+#' result <- siv_regression(data, Y, X, H, reps=5)
+#' iv1 <-(result$IV1)# a simple SIV
+#' iv2 <-(result$IV2)# a robust parametric SIV (RSIV-p)
+#' iv3 <-(result$IV3)# a robust non-parametric SIV (RSIV-n)
+#' summ.iv1 <- summary(iv1, diagnostics=TRUE)
+#' summ.iv2<- summary(iv2, diagnostics=TRUE)
+#' summ.iv3 <- summary(iv3, diagnostics=TRUE)
+#'result$citable ###  renders the CI table of beta estimates
+#'result$delta0 ###  renders the delta0 estimates:SIV, RSIV-p, RSIV-n.
 siv_regression <- function(data, Y, X, H, reps) {
   library(dplyr)
   library(ivreg)
@@ -22,78 +55,37 @@ siv_regression <- function(data, Y, X, H, reps) {
   library()
   rad2deg <- function(rad) {(rad * 180) / (pi)}
   deg2rad <- function(deg) {(deg * pi) / (180)}
-  # Two-sample Anderson-Darling statistic
-  # ad2_stat <- function(x, y) {
-  #   # Sample sizes
-  #   n <- length(x)
-  #   m <- length(y)
-  #
-  #   # Pooled sample and pooled ecdf
-  #   z <- c(x, y)
-  #   z <- z[-which.max(z)] # Exclude the largest point
-  #   H <- rank(z) / (n + m)
-  #
-  #   # Statistic computation via ecdf()
-  #   (n * m / (n + m)^2) * sum((ecdf(x)(z) - ecdf(y)(z))^2 / ((1 - H) * H))
-  #
-  # }
-  #
-  # check_sign_change <- function(x) {
-  #   # Step 2: Check if there is a sign change
-  #   signs <- sign(x)  # Get sign (-1, 0, or 1)
-  #   sign_changes <- any(diff(signs) != 0, na.rm = TRUE)  # Detect if sign changes
-  #
-  #   # Step 3: Return 1 if both conditions are met, otherwise 0
-  #   return(as.integer(sign_changes))
-  # }
-  #
-  #
-  # check_initial_abs_increase <- function(x) {
-  #   # Step 1: Check if the absolute values start by increasing (non-decreasing trend)
-  #   abs_x <- abs(x)
-  #   initial_increasing <- all(diff(abs_x[1:min(20, length(abs_x))]) >= 0)  # Check first 3 points or as many as available
-  #
-  #   # Step 3: Return 1 if both conditions are met, otherwise 0
-  #   return(as.integer(initial_increasing))
-  # }
-  # find_first_sign_change <- function(x) {
-  #   sign_changes <- which(diff(sign(x)) != 0)  # Find indices where sign changes
-  #   if (length(sign_changes) > 0) {
-  #     return(sign_changes[1] + 1)  # Return the first occurrence (adjust for diff)
-  #   } else {
-  #     return(NA)  # Return NA if no sign change
-  #   }
-  # }
-  # # Ensure variables exist in data
-  # if (!all(c(Y, X, H) %in% names(data))) {
-  #   stop("Some variables are missing in the dataset.")
-  # }
+  # Ensure variables exist in data
+  if (!all(c(Y, X, H) %in% names(data))) {
+    stop("Some variables are missing in the dataset.")
+  }
   # Y <- as.character(colnames(H0))[1] ###OUTCOME variable
   # X <-as.character(colnames(H0))[2] ###ENDOEGENOUS variable
   # H<- as.character(colnames(H0))[-(1:2)] ##### EXOGENOUS variables
+  #print(H)
   formula_str <- paste(paste0(Y," ~ ", X,"+"), paste(H, collapse = " + "))## Construct formula as a string
   formula <- as.formula(formula_str)# Convert to formula object
   #print(formula)
 
   # determine the number of rows
   N<-nrow(data)
-  #####Traditional methods
-
   #####SIV Method
 
   ###Basic vectors for  SIV calculation###
-  #y1<-hours ### the outcome variabe
+  # ### the outcome variable
   ## Factoring out the effects of other exogenous variables
   formula_str <- paste(paste0(Y," ~ "), paste(H, collapse = " + "))## Construct formula as a string
   formula <- as.formula(formula_str)# Convert to formula object
   fity<-lm(formula, data=data)
   y<-resid(fity)
-  #x1<-lwage### the endogenous variable
+  # the endogenous variable
   ## Factoring out the effects of other exogenous variables
   formula_str <- paste(paste0(X," ~ "), paste(H, collapse = " + "))## Construct formula as a string
   formula <- as.formula(formula_str)# Convert to formula object
   fitx<-lm(formula, data=data)
   x<-resid(fitx)
+  xx <- min(length((x-mean(x))),length((y-mean(y))))
+  data <- data[1:xx,]
   #saving the transformed x and y
   data$x<-(x-mean(x))
   data$y<-(y-mean(y))
@@ -106,33 +98,40 @@ siv_regression <- function(data, Y, X, H, reps) {
   V<-(V-mean(V))/sd(V)
   V<-V*sd(x0)
   data$R<-V
-
+  theta=0
+  theta1=0
+  d<-0.01# starting value for delta
+  delt<-0.01# step to change delta
+  while(theta1<70){
+    data$siv<-(data$x-d*data$R) ### SIV
+    theta <- acos( sum(data$x*data$siv) / ( sqrt(sum(data$x^2)) * sqrt(sum(data$siv^2)) ) )
+    theta1<-rad2deg(theta)
+    d=d+delt
+  }
+  dd <- d
+  dl=round(dd/delt)
   #####################################################################
   ############ Determining the sign of cor(x,u)
   k=0
   j=1
-
+  data$siv=0
   signc=matrix(ncol = 5, nrow = 2)
   signc[1,1] <-1
   signc[2,1] <--1
-  # ininc=matrix(ncol = 2, nrow = 2)
-  # ininc[1,1] <-1
-  # ininc[2,1] <--1
+  #
 
   for (j in 1:2) {
     if(j<2){k=1}else{k=-1}#the assumed sign for cor(x,u)
 
     # IV regression
-    theta=0
-    theta1=0
-    dd<-3#end value for delta
+    #dd<-3#end value for delta
     d<-0.01# starting value for delta
     delt<-0.01# step to change delta
     i<-1 ### starting value for a counter
     ## placeholders for variables
     # m1=0
     # m2=0
-    dl=round(dd/delt)
+    dl=round(dd/delt)-1
     m1 <- numeric(length = dl)  # Pre-allocate memory
     m2 <- numeric(length = dl)
     while (d<dd){# we compute m1 until siv is close to become perpendicular to x
@@ -148,17 +147,15 @@ siv_regression <- function(data, Y, X, H, reps) {
       d<-d+delt
       i<-i+1
     }
-    par(mfrow = c(1, 2))
 
-    plot(m1[0:300])
-    signc[j,3] <- check_initial_abs_increase(m1)#result
+    signc[j,3] <- check_initial_abs_increase(m1)#
     signc[j,5] <- check_initial_abs_increase(m2)
     if(signc[j,3]!=1){
-      index <- find_first_sign_change(m1)
+      index <- check_sign_change(m1)
       m=m1[index:i]
     }else{m=m1}
     signc[j,2] <-check_sign_change(m1)
-    signc[j,3] <- check_initial_abs_increase(m1)#result
+    signc[j,3] <- check_initial_abs_increase(m1)#
     signc[j,4] <- check_sign_change(m2)
   }
 
@@ -166,19 +163,11 @@ siv_regression <- function(data, Y, X, H, reps) {
   for(j in 1:2){
     ch[j] <- signc[j,2]+signc[j,3]+signc[j,4]+signc[j,5]
   }
-  # TRUE
-  # for(j in 1:2){cat("the assumed sign for cor(x,u):", signc[j,1], if(ch[j]-max(ch)!=0)
-  # {"is FALSE"
-  # }else{"is TRUE"},  "\n")
-  #   ch[j] <- signc[j,2]+signc[j,3]+signc[j,4]+signc[j,5]
-  # } # TRUE}}
 
   k <- signc[which.max(ch),1]
   cat("The true sign for cor(xu) is", k )
   #####################################
 
-  # if(signc[j,2]*signc[j,1]==0){"No endogeneity"}else{k=signc[j,1]}
-  #s<-1 # the assumed sign for cor(x,u)
   if(k!=0){
     ############# Initial settings for SIV
     vvar=0
@@ -190,7 +179,7 @@ siv_regression <- function(data, Y, X, H, reps) {
     b2rp=0
     b2t=0
     N<-nrow(data)
-    if (reps<2){reps=2} else{reps=reps}
+    reps=reps
     S=round(N*.999)
     l=1
     fitc <- matrix(ncol = 1, nrow = reps)
@@ -213,22 +202,22 @@ siv_regression <- function(data, Y, X, H, reps) {
       data_sample <- data[sample(1:N, S, replace = TRUE), ]
       ####Computation of m1
 
-      dd<-3#end value for delta
+      #dd<-3#end value for delta
       d<-0.01# starting value for delta
       delt<-0.01# step to change delta
       i<-1 ### starting value for a counter
       ## placeholders for variables
-      m1=0
+      m1=numeric(length = dl)  # Pre-allocate memory
       st=0
       ev22=0
       dv=0
-      dv2=0
-      x4=0
+      dv2=numeric(length = dl)
+      x4=numeric(length = dl)
       l1=0
       l2=0
       while (d<dd){# we compute m1 until siv is close to become perpendicular to x
         data_sample$siv<-(data_sample$x-k*d*data_sample$R)
-        #rls<-(lm(p401k~siv+inc+incsq+age+agesq+marr+fsize, data=data))
+        #
         rls<-(lm(x~siv, data=data_sample))
         s.rls<-summary(rls, diagnostics=T)
         data_sample$siv<-(data_sample$x-k*d*data_sample$R)
@@ -251,8 +240,8 @@ siv_regression <- function(data, Y, X, H, reps) {
         ssr2 <- sumsq(predict(lm((ev22^2)~siv,data=data_sample))-mean(data_sample$ev22^2))
         sse2=sumsq(data_sample$ev22)
         x2= (ssr2/2)/(sse2/n^2)^2
-        #dv[i] <-pchisq(x2, df =1,lower.tail=FALSE)-pchisq(x1, df =1,lower.tail=FALSE)#
-        x3 <- x1/x2#sumsq(predict(lm((ev22^2)~siv,data=data))/predict(lm((ev21^2)~siv,data=data))/predict(lm((ev21^2)~siv,data=data)))
+        #
+        x3 <- x1/x2#
         dv2[i] <- pf(x3, df = 1, df2 = 1, lower.tail = TRUE)
         st[i]<-d
         # non-parametric
@@ -271,14 +260,16 @@ siv_regression <- function(data, Y, X, H, reps) {
       ### update with your own instruments: instruments<-~ siv+all exogenous variables
       iv_str <- paste(paste0(" ~ ", "siv","+"), paste(H, collapse = " + "))## Construct formula as a string
       instruments <- as.formula(iv_str)# Convert to formula object
-      #instruments<-~siv+educ+ age+kidslt6+ kidsge6+ nwifeinc
+      #instruments<-~ siv+all exogenous variables
       # formula <-hours~lwage+educ+ age+kidslt6+ kidsge6+ nwifeinc
+      ###the First-Stage equation formula
+      fs_formula_str <- paste(paste0(X," ~ ","siv","+"), paste(H, collapse = " + "))#
       #### DT condition of homoscedatic case
       d0 <- (which.min(abs(m1)))*delt
       d0i[l] <- d0
       data_sample$siv<-(data_sample$x-k*d0*data_sample$R)
       iv2<-ivreg(formula, instruments, data=data_sample)
-      summ.iv2 <- summary(iv2, diagnostics=T)#, vcov. = function(x) vcovHC(x, type="HC1"), diagnostics=T)
+      summ.iv2 <- summary(iv2, diagnostics=T)#
       ## saving the estimation parameters for each sample
       fitc[l] <- iv2$coefficients[2]
       sumb2[l] <-  summ.iv2$coefficients[2,2]
@@ -288,7 +279,7 @@ siv_regression <- function(data, Y, X, H, reps) {
       d0ri[l] <- d0r
       data_sample$siv<-(data_sample$x-k*d0r*data_sample$R)
       iv3<-ivreg(formula, instruments, data=data_sample)
-      summ.iv3 <- summary(iv3, diagnostics=T)#,  vcov. = function(x) vcovHC(x, type="HC1"), diagnostics=T)
+      summ.iv3 <- summary(iv3, diagnostics=T)#,
       ## saving the estimation paramters for each sample
       fitcr[l] <- iv3$coefficients[2]
       sumb2r[l] <-  summ.iv3$coefficients[2,2]
@@ -299,7 +290,7 @@ siv_regression <- function(data, Y, X, H, reps) {
       data_sample$siv<-(data_sample$x-k*d0rn*data_sample$R)
       iv4<-ivreg(formula, instruments, data=data_sample)
       summ.iv4 <- summary(iv4, diagnostics=T)#,
-      ## saving the estimation paramters for each sample
+      ## saving the estimation parameters for each sample
       fitcrn[l] <- iv4$coefficients[2]
       sumb2rn[l] <-  summ.iv4$coefficients[2,2]
       l <- l+1
@@ -350,37 +341,41 @@ siv_regression <- function(data, Y, X, H, reps) {
     d0i <-  d0i[complete.cases(d0i)]
     d0m <- mean(d0i)
     data$siv<-(data$x-k*d0m*data$R)
-    iv2<-ivreg(formula, instruments, data=data)
-    summ.iv2 <- summary(iv2, diagnostics=T)#
+
+    fs1<-lm(as.formula(fs_formula_str), data=data)
+    iv1<-ivreg(formula, instruments, data=data)
+    #summ.iv1 <- summary(iv1, diagnostics=T)#
     ############Paramteric heterogenous case
     d0ri <-  d0ri[complete.cases(d0ri)]
     d0rm <- mean(d0ri)
     data$siv<-(data$x-k*d0rm*data$R)
-    iv3<-ivreg(formula, instruments, data=data)
-    summ.iv3 <- summary(iv2, diagnostics=T)#
+    fs2<-lm(as.formula(fs_formula_str), data=data)
+    iv2<-ivreg(formula, instruments, data=data)
+    #
 
     #### Non-parameteric heterogenous case
     d0rni <-  d0rni[complete.cases(d0rni)]
     d0rnm <- mean(d0rni)
     data$siv<-(data$x-k*d0rnm*data$R)
-    iv4<-ivreg(formula, instruments, data=data)
-    summ.iv4 <- summary(iv2, diagnostics=T)#
+    fs3<-lm(as.formula(fs_formula_str), data=data)
+    iv3<-ivreg(formula, instruments, data=data)
+    #
   }else{
     v=rnorm(N,0,1)
     print("NO endogeneity problem. All SIV estimates are the same as the OLS")
     d0m=0.001
     data$siv<-(data$x-k*d0m*data$R)+v
-    iv2<-ivreg(formula, instruments, data=data)
-    summ.iv2 <- summary(iv2, diagnostics=T)#
+    iv1<-ivreg(formula, instruments, data=data)
+    #
     d0rm=0.001
     data$siv<-(data$x-k*d0rm*data$R)+v
-    iv3<-ivreg(formula, instruments, data=data)
-    summ.iv3 <- summary(iv2, diagnostics=T)#
+    iv2<-ivreg(formula, instruments, data=data)
+    #
 
     d0rnm=0.001
     data$siv<-(data$x-k*d0rnm*data$R)+v
-    iv4<-ivreg(formula, instruments, data=data)
-    summ.iv4 <- summary(iv2, diagnostics=T)#
+    iv3<-ivreg(formula, instruments, data=data)
+    #
   }
-  return(list(IV2 = (iv2), IV3 = (iv3), IV4 = (iv4), citable=mv))
+  return(list(IV1 = (iv1), IV2 = (iv2), IV3 = (iv3), FS1=(fs1), FS2=(fs2), FS3=(fs3), citable=mv, delta0=c(d0m, d0rm, d0rnm)))
 }
